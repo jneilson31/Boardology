9df350534023689025899407e67c602ee4dfe323
Jeremy Neilson <jeremyneilson31@gmail.com>
2019-02-18 10:23:46 -0700

    initial boardology set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderPrimaryComponent } from './header/header-primary/header-primary.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPrimaryComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/header-primary/header-primary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-primary',
+  templateUrl: './header-primary.component.html',
+  styleUrls: ['./header-primary.component.scss']
+})
+export class HeaderPrimaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
