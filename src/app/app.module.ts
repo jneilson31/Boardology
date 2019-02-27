8def2e1e0fa6add77e58232284f14032def4ee80
Jeremy <jeremyneilson31@gmail.com>
2019-02-27 13:02:54 -0700

    adding component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderPrimaryComponent } from './header/header-primary/header-primary.component';
+import { HeaderPrimaryComponent } from './shared/header/header-primary/header-primary.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SearchComponent } from './shared/search/search.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductItemComponent } from './product/product-item/product-item.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { LoginComponent } from './shared/header/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderPrimaryComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    FooterComponent,
+    SearchComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
