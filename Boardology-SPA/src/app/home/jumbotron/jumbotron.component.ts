886c48c0be19c706469d7eb7dd3764e9b481197a
Jeremy Neilson <jeremyneilson31@gmail.com>
2019-03-23 19:26:43 -0600

    making log in dynamic

--- Boardology-SPA/src/app/home/jumbotron/jumbotron.component.ts
+++ Boardology-SPA/src/app/home/jumbotron/jumbotron.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-jumbotron',
   templateUrl: './jumbotron.component.html',
   styleUrls: ['./jumbotron.component.scss']
 })
+
 export class JumbotronComponent implements OnInit {
 
-  constructor() { }
+  userName: string;
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+  }
+
+  isLoggedIn(): boolean {
+    if (this.authService.loggedIn()) {
+      this.userName = this.authService.decodedToken.unique_name;
+      return true;
+    }
+    return false;
   }
 
 }
--- Boardology-SPA/src/app/shared/header/login/login.component.ts
+++ Boardology-SPA/src/app/shared/header/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  isLoggedIn() {
+    return this.authService.loggedIn();
+  }
+
+  onLogout() {
+   this.authService.logout();
+
   }
 
 }
