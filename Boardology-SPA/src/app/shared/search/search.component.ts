e85529df905bd3198c96354cc6986ea7aa9f76fc
Ninnon <trentonneilson14@hotmail.com>
2019-04-19 20:47:05 -0600

    implement fuse

--- Boardology-SPA/src/app/shared/search/search.component.ts
+++ Boardology-SPA/src/app/shared/search/search.component.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
 import { Product } from '../../_models/product.model';
 import { AlertifyService } from '../../_services/alertify.service';
+import { ProductService } from '../../_services/product.service';
+import Fuse from 'fuse.js';
 
 @Component({
   selector: 'app-search',
@@ -13,28 +15,52 @@ import { AlertifyService } from '../../_services/alertify.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
-  products: Product[] = [];
+  options: Fuse.FuseOptions<Product> = {
+    keys: ['name']
+  };
+  products: any;
+  originalProducts: any;
   queryField: FormControl = new FormControl();
   hasSearchResults = false;
   isDoneSearching = false;
-  constructor(private searchService: SearchService, private alertify: AlertifyService) { }
+  constructor(private searchService: SearchService, private alertify: AlertifyService, private productService: ProductService) { }
 
   ngOnInit() {
+
+    this.productService.getProducts()
+      .subscribe(products => {
+        this.originalProducts = products;
+      });
+    // this.searchService.search('labore');
     this.queryField.valueChanges
     .debounceTime(350)
     .distinctUntilChanged()
-    .switchMap(query => this.searchService.search(query))
+    .switchMap(query => this.search(query))
     .subscribe(results => {
-        this.products = results;
-        this.hasSearchResults = this.products.length ? true : false;
-        this.isDoneSearching = true;
-        if (this.queryField.value === '') {
-          this.hasSearchResults = false;
-          this.isDoneSearching = false;
-        }
-      }, error => {
-        this.alertify.error('Could not search!');
-      });
+      this.products = results;
+    });
+
+    // .subscribe(results => {
+    //     this.products = results;
+    //     this.hasSearchResults = this.products.length ? true : false;
+    //     this.isDoneSearching = true;
+    //     if (this.queryField.value === '') {
+    //       this.hasSearchResults = false;
+    //       this.isDoneSearching = false;
+    //     }
+    //   }, error => {
+    //     this.alertify.error('Could not search!');
+    //   });
   }
-}
+
+  private search(queryString: string): any {
+        const fuse = new Fuse(this.originalProducts, this.options);
+        const results = fuse.search(queryString);
+        const myResults = new Array(results);
+        return myResults;
+      }
+
+
+    // return this.http.get<Product[]>(this.url + queryString);
+  }
+
--- Boardology-SPA/src/app/shared/search/search.service.ts
+++ Boardology-SPA/src/app/shared/search/search.service.ts
@@ -4,16 +4,35 @@ import 'rxjs/add/operator/map';
 import { Product } from '../../_models/product.model';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { ProductService } from '../../_services/product.service';
+import Fuse from 'fuse.js';
+ // import * as Fuse from 'fuse.js';
 
 @Injectable()
 export class SearchService {
     baseUrl = environment.apiUrl;
     url = `${this.baseUrl}games/search?search=`;
+    products: any;
+    options: Fuse.FuseOptions<Product> = {
+        keys: ['name']
+    };
 
-    constructor(private http: HttpClient) { }
 
-    search(queryString: string): Observable<any> {
-        return this.http.get<Product[]>(this.url + queryString);
+    constructor(private http: HttpClient, private productService: ProductService) { }
+
+    search(queryString: string): any {
+        this.productService.getProducts()
+        .subscribe(products => {
+            this.products = products;
+            console.log(this.products);
+            const fuse = new Fuse(this.products, this.options);
+            const results = fuse.search(queryString);
+            console.log(results);
+            return results;
+        });
+
+
+        // return this.http.get<Product[]>(this.url + queryString);
     }
 }
 
