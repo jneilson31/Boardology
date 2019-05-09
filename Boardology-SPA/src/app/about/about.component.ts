69e1a290148c01bccc47555cfeca91294052092d
Jeremy <jeremyneilson31@gmail.com>
2019-05-09 00:53:49 -0600

    home page changes

--- Boardology-SPA/src/app/about/about.component.ts
+++ Boardology-SPA/src/app/about/about.component.ts
@@ -3,35 +3,27 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 import { Product } from '../_models/product.model';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
-  animations: [
-    trigger('flipState', [
-      state('sideB', style({
-        transform: 'rotateY(180deg)'
-      })),
-      state('sideA', style({
-        transform: 'rotateY(0)'
-      })),
-      transition('sideB => sideA', animate('500ms ease-out')),
-      transition('sideA => sideB', animate('500ms ease-in'))
-    ])
-  ]
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss'],
+    animations: [
+        trigger('flipState', [
+            state('sideB', style({
+                transform: 'rotateY(180deg)'
+            })),
+            state('sideA', style({
+                transform: 'rotateY(0)'
+            })),
+            transition('sideB => sideA', animate('500ms ease-out')),
+            transition('sideA => sideB', animate('500ms ease-in'))
+        ])
+    ]
 })
 
 export class AboutComponent implements OnInit {
-  @Input() product: Product;
 
-  cardFlip: string = 'sideA';
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  toggleFlip() {
-    console.log("clicked");
-    this.cardFlip = (this.cardFlip === 'sideA') ? 'sideB' : 'sideA';
-  }
+    ngOnInit() {
+    }
 }
