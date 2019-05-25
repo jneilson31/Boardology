f326c2a5bbea5cee40a3792e2add57b1e4ffb4a6
Ninnon <trentonneilson14@hotmail.com>
2019-05-25 11:31:22 -0600

    adds ability to remove from collection and wishlist - though it is ugly

--- Boardology-SPA/src/app/wishlist/wishlist.component.ts
+++ Boardology-SPA/src/app/wishlist/wishlist.component.ts
@@ -4,6 +4,7 @@ import { ProductService } from '../_services/product.service';
 import { WishlistProduct } from '../_models/wishlist-product.model';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-wishlist',
@@ -14,7 +15,7 @@ export class WishlistComponent implements OnInit {
   productWishlist: WishlistProduct[] = [];
   baseUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private http: HttpClient, private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.getWishlist();
@@ -28,5 +29,17 @@ export class WishlistComponent implements OnInit {
       .subscribe(wishlist => {
         this.productWishlist = wishlist;
       });
+  }
+
+  removeFromWishlist(productId: number): void {
+    this.alertify.confirm('Are you sure you want remove this game from your wishlist?', 'Yes', undefined, () => {
+      this.http.delete(`${this.baseUrl}games/${this.authService.decodedToken.nameid}/${productId}/wishlist`)
+        .subscribe(results => {
+          const productToRemove = this.productWishlist.findIndex(x => x.id === productId);
+          this.productWishlist.splice(productToRemove, 1);
+        }, error => {
+          console.log(error);
+        });
+    });
   }
 }
